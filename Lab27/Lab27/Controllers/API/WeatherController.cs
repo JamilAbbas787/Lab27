7b1bf0df2cd181b3e0f7a065749429d0352175f3
Jamil Abbas <jamilabbas787@gmail.com>
2019-02-10 22:40:56 -0500

    pulled weather api into view

---
+++ Lab27/Lab27/Controllers/API/WeatherController.cs
@@ -0,0 +1,27 @@
+﻿using Lab27.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lab27.Controllers.API
+{
+    public class WeatherController : ApiController
+    {
+        private Weather _context = new Weather(); 
+
+        // GET: api/Weather/5
+        //public string Get()
+        //{
+        //    return HttpContent($"/api/weather");
+        //    //return "value";
+        //}
+
+        // POST: api/Weather
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
---
+++ Lab27/Lab27/Models/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab27.Models
+{
+    public class Weather
+    {
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+    }
+}
